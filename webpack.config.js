f8ba4a3ba2d0b59edae83ad9ffe9291843473c2d
Arturo Montoya <montoya@Arturos-Mac-mini.local>
2017-02-08 14:40:21 -0800

    Updated: Packages

--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,57 @@ var webpack = require('webpack');
 var assetPath = '/assets/';
 var absolutePath = path.join(__dirname, 'build', assetPath);
 
-module.exports = {
-    devtool: 'source-map',
-    entry: [
-        'babel-polyfill',
-        './src/index.js'
-    ],
-    output: {
-        path: absolutePath,
-        publicPath: assetPath,
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            }
-        }),
-    ],
-    module: {
-        loaders: [{
+module.exports = (env, argv) => {
+    return {
+        devtool: 'source-map',
+        entry: [
+            'babel-polyfill',
+            './src/index.js'
+        ],
+        output: {
+            path: absolutePath,
+            publicPath: assetPath,
+            filename: 'bundle.js'
+        },
+        plugins: [],
+        module: {
+            rules: [
+            {
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015', 'stage-1']
-                }
+                use: [{
+                    loader: 'react-hot-loader'
+                }, {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015', 'react', 'stage-1']
+                    }
+                }]
             }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './',
-        port: 8081
+        },
+        resolve: {
+            extensions: [ '.js', '.jsx']
+        },
+        devServer: {
+            historyApiFallback: true,
+            contentBase:  './',
+            port: argv.port || 8081,
+            hot: true,
+            stats: {
+                cached: false,
+                colors: true,
+                hash: false,
+                version: false,
+                timings: false,
+                assets: false,
+                chunks: false,
+                modules: false,
+                reasons: true,
+                source: true,
+                errors: true,
+                errorDetails: true,
+                warnings: true,
+            }
+        }
     }
 };

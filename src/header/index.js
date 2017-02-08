939f8c8333f0624c36c57656416375419748fe41
Arturo Montoya <montoya@Arturos-Mac-mini.local>
2017-02-08 15:08:19 -0800

    Added: Material UI

--- src/header/index.js
+++ src/header/index.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import AppBar from 'material-ui/AppBar';
 
-const links = [{id: '2', title:'Library', href: '/library'}]
 
 export default class Navigation extends Component {
-    renderLinks() {
-          return links.map(function(item) {
-                  return (
-                    <li key={item.id}><Link className="white-text" to={item.href}>{item.title}</Link></li>
-                  );
-                });
-    }
-
-
     render() {
         return (
-        	<nav className="teal" role="navigation">
-                <div className="nav-wrapper container">
-                    <Link id="logo-container" to="#" className="brand-logo white-text">React Examples</Link>
-                    <ul id="nav-desktop" className="right hide-on-med-and-down">
-                       {this.renderLinks()}
-                    </ul>
-
-                    <ul id="nav-mobile" className="side-nav">
-                        {this.renderLinks()}
-                    </ul>
-                    <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
-                </div>
-            </nav>
+        	<AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import ReduxPromise from 'redux-promise';
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import reducers from './rootReducer';
 import routes from './routes';
+
+injectTapEventPlugin();
 
 const createStoreWithMiddleware = compose(applyMiddleware(
     ReduxPromise
@@ -15,7 +18,9 @@ const createStoreWithMiddleware = compose(applyMiddleware(
   : f => f)(createStore)
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} routes={routes}/>
-  </Provider>
+	<MuiThemeProvider>
+  		<Provider store={createStoreWithMiddleware(reducers)}>
+    		<Router history={browserHistory} routes={routes}/>
+  		</Provider>
+  	</MuiThemeProvider>
 , document.querySelector('#App'));

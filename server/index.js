400daf81a9122806f9402a4228a31075855350f0
Arturo Montoya <montoya332@live.com>
2017-03-06 15:46:36 -0800

    Added: Routes

--- server/index.js
+++ server/index.js
@@ -4,23 +4,17 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 /* App Setup */
 app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));//TODO: remove wildcard , force json for now
+app.use(bodyParser.json({ type: '*/*' })); //TODO: remove wildcard , force json for now
+routes(app);
 
 /* Server Setup */
 const port = process.env.PORT || 3000
 const server = http.createServer(app);
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`)
-})
-
-app.get('/', function (req, res) {
-  res.send('Hello World! /')
-})
-
-app.get('/route', function (req, res) {
-  res.send('Hello route!')
+app.listen(port, function() {
+	console.log(`Example app listening on port ${port}!`)
 })

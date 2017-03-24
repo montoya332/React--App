8474f5bf4e4a62f1770d344f6a2337c9266ebc5a
Arturo Montoya <montoya332@live.com>
2017-03-24 12:37:00 -0700

    Added: authenticate test

--- client/src/socket_io_Test.js
+++ client/src/socket_io_Test.js
@@ -1,14 +1,16 @@
 import hooks from 'feathers-hooks';
 import feathers from 'feathers/client';
 import socketio from 'feathers-socketio/client';
-// import io from 'socket.io-client/socket.io';
+import authentication from 'feathers-authentication/client';
 import io from 'socket.io-client';
 if(!global._babelPolyfill) { require('babel-polyfill'); }
 
 const socket = io('http://localhost:3030', { transports: ['websocket'] });
+
 const app = feathers()
-  // .configure(feathers.hooks())
-  .configure(socketio(socket));
+  .configure(socketio(socket))
+  .configure(hooks())
+  .configure(authentication({ storage: window.localStorage }));
  var userService = app.service('users');
  userService.on('updated', function(data) {
     console.log('Someone updated a message', data);
@@ -24,3 +26,13 @@ const app = feathers()
 });
 window.userService = userService
 userService.find().then(function(data){console.log('Users: ',data)})
+
+app.authenticate({
+  type: 'local',
+  'email': 'montoya332@live.com',
+  'password': 'test123'
+}).then(function(result){
+  console.log('Authenticated!', result);
+}).catch(function(error){
+  console.error('Error authenticating!', error);
+})

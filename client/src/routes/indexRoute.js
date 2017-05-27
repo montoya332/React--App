4e2d7e8bbabefd546eed0b903c6eaa32c4e25ac9
Arturo Montoya <montoya332@live.com>
2017-05-26 19:46:20 -0700

    Modified: Routes , Code Clean up

--- client/src/routes/indexRoute.js
+++ client/src/routes/indexRoute.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import {
 	browserHistory,
 	BrowserRouter as Router,
@@ -8,60 +8,62 @@ import {
 	Link,
 	Redirect,
 	withRouter
-} from 'react-router-dom'
-import { Switch } from 'react-router'
-import App from '../components/app'
-import {LibraryContainer} from '../components/library/index'
-import ErrorPage404 from '../components/general/404error'
-import ChatApp from '../components/messages/container/chatContainer'
-import { app } from 'utils/socketio'
-import {LoginContainer, UserProfileContainer} from 'components/userProfile'
-import createHistory from 'history/createBrowserHistory'
-const history = createHistory()
-export const PublicRoutes = ({store}) => {
+} from 'react-router-dom';
+import { Switch } from 'react-router';
+import App from '../components/app';
+import {LibraryContainer} from '../components/library/index';
+import ErrorPage404 from '../components/general/404error';
+import ChatApp from '../components/messages/container/chatContainer';
+import { app } from 'utils/socketio';
+import {LoginContainer, UserProfileContainer} from 'components/userProfile';
+import createHistory from 'history/createBrowserHistory';
+const history = createHistory();
 
-	return (
-			<App>
-				<Switch>
-					<Route  path="/profile" component={UserProfileContainer}/>
-					<Route exact path="/library" component={LibraryContainer}/>
-					<Route path="/login" component={LoginContainer}/>
-					<Redirect to={{  pathname: '/login' }}/> 
-				</Switch>
-			</App>
-	)
-}
-export const PrivateRoutes = (props) => {
-	return (
-		<App>
-			<Switch>
-				<Route  path="/profile" component={UserProfileContainer}/>
-				<Route exact path="/library" component={LibraryContainer}/>
-				<Route path="/login" component={LoginContainer}/>
-				<Route exact path="/messages" component={ChatApp}/>
-				<Route component={ ErrorPage404 }/>
-			</Switch>
-		</App>
-	)
-}
-export const AuthPrivateRoutes = withRouter(({ history,store,...rest }) => {
-	//store.getState().clientUser.get('token')
-  return localStorage.getItem('feathers-jwt') ? (
-    <PrivateRoutes {...rest} />
+
+export const SmallProjectRoutes = ({store}) => (
+	<App>
+	</App>
+	);
+
+
+
+export const PublicRoutes = ({store}) => (
+	<App>
+		<Switch>
+			<Route path="/profile" component={UserProfileContainer}/>
+			<Route exact path="/library" component={LibraryContainer}/>
+			<Route path="/login" component={LoginContainer}/>
+			<Redirect to={{ pathname: '/login' }}/>
+		</Switch>
+	</App>
+	);
+export const PrivateRoutes = props => (
+	<App>
+		<Switch>
+			<Route path="/profile" component={UserProfileContainer}/>
+			<Route exact path="/library" component={LibraryContainer}/>
+			<Route path="/login" component={LoginContainer}/>
+			<Route exact path="/messages" component={ChatApp}/>
+			<Route component={ErrorPage404}/>
+		</Switch>
+	</App>
+	);
+export const AuthPrivateRoutes = withRouter(({ history, store, ...rest }) => {
+	// store.getState().clientUser.get('token')
+	return (<SmallProjectRoutes {...rest} />);
+	return localStorage.getItem('feathers-jwt') ? (
+		<PrivateRoutes {...rest} />
   ) : (
-    <PublicRoutes {...rest} />
-  )
-})
-export const AppRoutes = (store) => {
-	return (
-		<Router history={history}>
-			<div>
-				<AuthPrivateRoutes store={store}/>
-			</div>
-		</Router>
-    )
-}
+	<PublicRoutes {...rest} />
+  );
+});
+export const AppRoutes = store => (
+	<Router history={history}>
+		<div>
+			<AuthPrivateRoutes store={store}/>
+		</div>
+	</Router>
+    );
 
-export default AppRoutes
-
+export default AppRoutes;
 
